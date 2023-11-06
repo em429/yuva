3a22f31dfdf90688a64b1d906352c826a6dc1d0d
qirpi <github.login@mem.cool>
2023-11-07 00:29:32 +0100

    validates

--- app/models/shopping_item.rb
+++ app/models/shopping_item.rb
@@ -5,6 +5,8 @@ class ShoppingItem < ApplicationRecord
 
   validates :stock, numericality: { greater_than_or_equal_to: 0 }
   validates :title, presence: true
+  validates :unit, presence: true
+  validates :importance, presence: true
 
   scope :cooking_ingredients, -> { where(shopping_category_id: 1) }
 
