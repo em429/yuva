3673cd5e12b223b47cd95a10165792a99bb7da8b
qirpi <github.login@mem.cool>
2023-11-06 15:42:28 +0100

    add dependent:destroy to join table

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-    has_many :recipe_items
+    has_many :recipe_items, dependent: :destroy
     has_many :shopping_items, through: :recipe_items
 
     validates :title, presence: true
--- app/models/shopping_item.rb
+++ app/models/shopping_item.rb
@@ -1,6 +1,6 @@
 class ShoppingItem < ApplicationRecord
   belongs_to :shopping_category
-  has_many :recipe_items
+  has_many :recipe_items, dependent: :destroy
   has_many :recipes, through: :recipe_items
 
   validates :stock, numericality: { greater_than_or_equal_to: 0 }
