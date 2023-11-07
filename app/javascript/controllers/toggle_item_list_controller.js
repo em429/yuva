f9106fff7dbaf0a7bf132fa15c4fc1f83133b425
qirpi <github.login@mem.cool>
2023-11-07 14:40:26 +0100

    restyle dahsboard page, refactor toggle stimulus controllers

--- app/javascript/controllers/toggle_item_list_controller.js
+++ app/javascript/controllers/toggle_item_list_controller.js
@@ -2,15 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-item-list"
 export default class extends Controller {
-  static targets = ["list"]
-
-  connect() {
-    console.log("asd")
-  }
+  static targets = ["list", "title"]
 
   toggleList() {
-    console.log("toggle")
     this.listTarget.classList.toggle('hidden')
+    
+    // Add shadow to category title box after hiding the list
+    this.titleTarget.classList.toggle('shadow-md')
+    this.titleTarget.classList.toggle('shadow-stone')
   }
 
 }
