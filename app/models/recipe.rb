98298efb4cae5402deb0032d1b3f1bd478cd0b76
qirpi <github.login@mem.cool>
2023-11-17 15:03:13 +0100

    add uniqueness validation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,6 @@ class Recipe < ApplicationRecord
     has_many :shopping_items, through: :recipe_items
 
     validates :title, presence: true
+    validates_uniqueness_of :title
     validates :description, presence: true
 end
